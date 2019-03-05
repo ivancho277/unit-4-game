5bd556aac551fc99e7d5ffffa161419cd1fe5b95
ivancho277 <rouskoi@live.com>
2019-03-04 19:55:57 -0800

    wip

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$(document).ready(function) {
+
+let characters = {
+    "geralt" : {
+        name: "geralt",
+        health: 120,
+        attack: 8,
+        counterAttack: 15,
+        imageUrl: "assets/images/geralt.jpg"
+    },
+    "vesemir" : {
+        name: "vesemir",
+        health: 100,
+        attack: 14,
+        counterAttack: 5,
+        imageUrl: "assets/images/vesemir.jpg"
+    },
+    "ciri" : {
+        name: "ciri",
+        health: 150,
+        attack: 8,
+        counterAttack: 20,
+        imageUrl: "assets/images/ciri.jpg"
+    },
+    imlerith : {
+        name: "imlerith",
+        health: 180,
+        attack: 7,
+        counterAttack: 20,
+        imageUrl: "assets/images/imlerith.jpg"
+    }
+};
+
+
+
+
+
+
+
+
+
+
+}
