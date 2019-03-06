a121ad02a1e13e70515f2e05c5457039621643f8
ivancho277 <rouskoi@live.com>
2019-03-05 18:47:01 -0800

    a few changes to changing border color and selction sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,28 +6,32 @@ let characters = {
         health: 120,
         attack: 8,
         counterAttack: 15,
-        imageUrl: "assets/images/geralt.jpg"
+        imageUrl: "assets/images/geralt.jpg",
+        isSelected: false
     },
     "vesemir" : {
         name: "vesemir",
         health: 100,
         attack: 14,
         counterAttack: 5,
-        imageUrl: "assets/images/vesemir.jpg"
+        imageUrl: "assets/images/vesemir.jpg",
+        isSelected: false
     },
     "ciri" : {
         name: "ciri",
         health: 150,
         attack: 8,
         counterAttack: 20,
-        imageUrl: "assets/images/ciri.jpg"
+        imageUrl: "assets/images/ciri.jpg",
+        isSelected: false
     },
     "imlerith" : {
         name: "imlerith",
         health: 180,
         attack: 7,
         counterAttack: 20,
-        imageUrl: "assets/images/imlerith.jpg"
+        imageUrl: "assets/images/imlerith.jpg",
+        isSelected: false
     }
 };
 
@@ -37,11 +41,30 @@ let attackSound = new Audio('assets/sound/SwordsCollide.mp3');
 let deathSound = new Audio('assets/sound/Decapitation.mp3');
 let winningSound = new Audio('assets/sound/TaDa.mp3');
 
+
+
+$(function () {
+    $("img").click(function() {
+      $(this).css('border', "solid 5px greenyellow");  
+    });
+  });
+
+function onImgClick(){
+    $("img").click(function() {
+        console.log("image clicked");
+        attackSound.play();
+    })
+}
+
 $("#geralt-img").click(function (e) { 
-    console.log("clicked geralt!")
-    attackSound.play();
-    
+    console.log("clicked geralt!");
+    characters.isSelected = true;
+    onImgClick();
 });
+
+
+
+
 
 
 
